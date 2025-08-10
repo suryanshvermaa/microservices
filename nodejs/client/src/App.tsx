ec5f4ae1df426e33153760b9f6cfa67af2029e35
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-08-10 12:32:53 +0530

    added api requets

--- nodejs/client/src/App.tsx
+++ nodejs/client/src/App.tsx
@@ -1,13 +1,44 @@
+import { AlertDialog, AlertDialogContent } from '@radix-ui/react-alert-dialog'
 import './App.css'
 import CreateSnippet from './components/CreateSnippet'
 import Navbar from './components/Navbar'
-
+import { useEffect, useState } from 'react';
+import { Input } from './components/ui/input';
+import { Button } from './components/ui/button';
 function App() {
+    const [userId,setUserId]=useState<number|undefined>();
+    const [getUserId,setGetUserId]=useState<boolean>(false);
+    useEffect(()=>{
+    const userId=localStorage.getItem("userId");
+    if(userId){
+      setUserId(parseInt(userId));
+    }else{
+      setGetUserId(true);
+    }
+  },[])
 
+  const handleUserId=()=>{
+    if(!userId) return;
+    localStorage.setItem("userId",userId.toString());
+    setGetUserId(false);
+  }
   return (
       <main>
         <Navbar/>
-        <CreateSnippet/>
+        <CreateSnippet userId={Number(userId)}/>
+        {
+          getUserId && <div style={{position:"absolute",top:0,left:0,right:0,bottom:0}} className='h-screen w-screen backdrop-blur-md bg-gray-300/50 dark:bg-gray-800/50 flex justify-center items-center'>
+                <AlertDialog defaultOpen>
+                  <AlertDialogContent>
+                    <h1 className='font-bold text-2xl mb-8'>User Snippet Plateform</h1>
+                    <div className='flex flex-col gap-2'>
+                      <Input value={userId} placeholder='Enter User Id...' onChange={(e)=>setUserId(parseInt(e.target.value))}/>
+                      <Button onClick={handleUserId}>Submit</Button>
+                    </div>
+                  </AlertDialogContent>
+                </AlertDialog>
+        </div>
+        }
       </main>
   )
 }
--- nodejs/client/src/components/CreateSnippet.tsx
+++ nodejs/client/src/components/CreateSnippet.tsx
@@ -2,16 +2,77 @@ import React from 'react'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
+import axios, { AxiosError } from "axios";
+import { Alert, AlertTitle } from './ui/alert';
+import { AlertCircleIcon, CheckCircle2Icon } from 'lucide-react';
 
-const CreateSnippet: React.FC = () => {
+interface ISnippet{
+  title?: string,
+  code?: string,
+  description?: string,
+  userId?: number
+}
+
+interface IAlert{
+  alert: boolean,
+  success: boolean,
+  message: string,
+}
+
+
+const CreateSnippet: React.FC = ({userId}:{userId?: number} ) => {
+  const [alert,setAlert]=React.useState<IAlert>({alert:false,success:false,message:""});
+  const [snippet,setSnippet]=React.useState<ISnippet|undefined>();
+  const createSnippet=async(e:React.FormEvent<HTMLFormElement>)=>{
+    try {
+      e.preventDefault();
+      if(!snippet) return;
+      snippet.userId=userId as number;
+      const res=await axios.post("http://localhost:3000/api/v1/snippet",{
+        ...snippet
+      });
+      setAlert({
+        alert:true,
+        success:true,
+        message:res.data.message
+      });
+    } catch (error: AxiosError | any) {
+      if(error instanceof AxiosError){
+        setAlert({
+          alert:true,
+          success:false,
+          message:error.response?.data.message
+        });
+        return;
+      }
+      setAlert({
+          alert:true,
+          success:false,
+          message:error?.message
+        });
+    }
+  }
   return (
     <div>
-        <form className='mt-10 flex flex-col gap-4'>
-            <Input type='text' placeholder='write a code snippets...'/>
-            <Textarea placeholder='write a code snippets...'/>
+        <form className='mt-10 flex flex-col gap-4' onSubmit={createSnippet}>
+            <Input type='text' placeholder='write a code snippets...' value={snippet?.title} onChange={(e)=>setSnippet({...snippet,title:e.target.value})}/>
+            <Textarea placeholder='write a code snippets...' value={snippet?.code} onChange={(e)=>setSnippet({...snippet,code:e.target.value})}/>
+            <Textarea placeholder='write a description...' value={snippet?.description} onChange={(e)=>setSnippet({...snippet,description:e.target.value})}/>
             <div className='flex justify-start'>
-                <Button>Add</Button>
+                <Button type='submit'>Add</Button>
             </div>
+            {
+              alert.alert&&
+                  alert.success&&<Alert>
+                      <CheckCircle2Icon />
+                      <AlertTitle>{alert.message}</AlertTitle>
+                    </Alert>
+            }{
+                alert.alert && alert.success===false&&<Alert variant={"destructive"}>
+                      <AlertCircleIcon />
+                      <AlertTitle>{alert.message}</AlertTitle>
+                    </Alert>
+            }
         </form>
     </div>
   )
