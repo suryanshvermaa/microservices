b74ff556a2f6cd7ded50c9cf6d343c918e9441e1
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-08-10 10:04:04 +0530

    initialied code snipped client side project with react tailwind and shadcn

---
+++ nodejs/client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+      <main>
+        <Navbar/>
+      </main>
+  )
+}
+
+export default App
---
+++ nodejs/client/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>Code Snippet</h1>
+        <button>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
