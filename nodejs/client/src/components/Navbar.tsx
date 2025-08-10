882742692bfaf1a5d28d28208f88ea689ce65006
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-08-10 10:07:44 +0530

    added navbar

--- nodejs/client/src/components/Navbar.tsx
+++ nodejs/client/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from './ui/button'
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
-    <div>
-        <h1>Code Snippet</h1>
-        <button>Logout</button>
+    <div className='flex justify-between'>
+        <h1 className='font-bold text-2xl'>Code Snippet</h1>
+        <Button>Logout</Button>
     </div>
   )
 }
