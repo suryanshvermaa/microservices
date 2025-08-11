7aa1ff64b408822f87bb06b1bfcb4e64a4b2b175
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-08-11 15:10:23 +0530

    updated resolvers

--- golang/services/graphql/query_resolvers.go
+++ golang/services/graphql/query_resolvers.go
@@ -1,9 +1,15 @@
 package main
 
+import "context"
+
 type queryResolver struct {
 	server *Server
 }
 
-func (r *queryResolver) accounts(pagination PaginationInput, id string) {
+func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]Account, error) {
+
+}
+
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, id *string) ([]Product, error) {
 
 }
