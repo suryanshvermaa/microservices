4b90b8c4dd06011bf215b19739ed753e1d5a10f3
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-12 11:00:03 +0000

    feat: update account service with protobuf definitions and gRPC methods for account management

--- golang/services/account/server.go
+++ golang/services/account/server.go
@@ -5,45 +5,60 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/suryanshvermaa/microservices/golang/services/account/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-type grpcServer struct{
+type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
 	service Service
 }
 
-func ListenGRPC(s Service,port int)error{
-	list,err:=net.Listen("tcp",fmt.Sprintf(":%d",port))
-	if err!=nil{
+func ListenGRPC(s Service, port int) error {
+	list, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
 		return err
 	}
-	serv:=grpc.NewServer()
-	pb.(serv,)
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcServer{service: s})
 	reflection.Register(serv)
 	return serv.Serve(list)
 }
 
-func (s *grpcServer) PostAccount(ctx context.Context,r *pb.) (*pb.,error){
-	a,err:=s.service.PostAccount(ctx,r.Name)
-	if err!=nil{
-		return nil,err
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
+	a, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
 	}
-	return &pb.{}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id:   a.ID,
+		Name: a.Name,
+	}}, nil
 }
 
-func (s *grpcServer) GetAccount(ctx context.Context, r *pb.) (*pb., error){
-	a,err:=s.service.GetAccount(ctx,r.Id)
-	if err!=nil{
-		return nil,err
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	a, err := s.service.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
 	}
-	return &pb.{}
+	return &pb.GetAccountResponse{Account: &pb.Account{
+		Id:   a.ID,
+		Name: a.Name,
+	}}, nil
 }
 
-func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.) (*pb., error){
-	a,err:=s.service.GetAccounts(ctx,r.skip,r.take)
-	if err!=nil{
-		return nil,err
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.GetAccountsRequest) (*pb.GetAccountsResponse, error) {
+	res, err := s.service.GetAccounts(ctx, r.Skip, r.Take)
+	if err != nil {
+		return nil, err
 	}
-	return &pb.{}
+	accounts := []*pb.Account{}
+	for _, p := range res {
+		accounts = append(accounts, &pb.Account{
+			Id:   p.ID,
+			Name: p.Name,
+		})
+	}
+	return &pb.GetAccountsResponse{Account: accounts}, nil
 }
