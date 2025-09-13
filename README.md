8b44804e9af0a5084abd987bc9b3715b3ccac680
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-13 16:54:35 +0000

    feat: add gRPC server implementation and protobuf definitions for catalog service

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Microservices using Go
 This repository contains a collection of microservices built using Go. Each service is designed to be independent and can communicate with others over HTTP or gRPC.
+
+# Protobuf generate command
+```
+protoc --go_out=. --go-grpc_out=. --proto_path=.  *.proto
+```
--- golang/services/catalog/server.go
+++ golang/services/catalog/server.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/suryanshvermaa/microservices/golang/services/catalog/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type grpcServer struct {
+	pb.UnimplementedCatalogServiceServer
 	service Service
 }
 
@@ -17,4 +20,7 @@ func ListenGRPC(s Service, port int) error {
 		return err
 	}
 	srv := grpc.NewServer()
+	pb.RegisterCatalogServiceServer(srv, &grpcServer{service: s})
+	reflection.Register(srv)
+	return srv.Serve(lis)
 }
