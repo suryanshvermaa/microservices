46ef27b6090ac896099d181bae980291f084d388
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-13 16:47:04 +0000

    feat: implement initial gRPC server setup and catalog service definitions

---
+++ golang/services/catalog/server.go
@@ -0,0 +1,20 @@
+package catalog
+
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type grpcServer struct {
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	srv := grpc.NewServer()
+}
