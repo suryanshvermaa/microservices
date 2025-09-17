4be6128f30b84fa00ad93ff7562625a6e11f8016
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-17 21:22:55 +0000

    feat: enhance PutOrder method with transaction handling and insert logic for ordered products

--- golang/services/order/respository.go
+++ golang/services/order/respository.go
@@ -3,6 +3,8 @@ package order
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
 type Repository interface {
@@ -24,13 +26,48 @@ func NewPostgresRepository(url string) (Repository, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &postgresRepository{db}
+	return &postgresRepository{db: db}
 }
 
 func (r *postgresRepository) Close() {
 	r.db.Close()
 }
 
-func (r *postgresRepository) PutOrder(ctx context.Context, o Order) error {
+func (r *postgresRepository) PutOrder(ctx context.Context, o Order) (err error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+	tx.ExecContext(
+		ctx,
+		"INSERT INTO orders(id,created_at,account_id,total_price) VALUES ($1,$2,$3,#4)",
+		o.ID,
+		o.CreatedAt,
+		o.AccountID,
+		o.TotalPrice,
+	)
+	if err != nil {
+		return err
+	}
+	stmt, _ := tx.PrepareContext(ctx, pq.CopyIn("ordered_products", "order_id", "product_id", "quantity"))
+	for _, p := range o.Products {
+		_, err = stmt.ExecContext(ctx, o.ID, p.Id, p.quantity)
+	}
+	stmt.ExecContext(ctx)
+	if err != nil {
+		return
+	}
+	stmt.Close()
+	return
+}
+
+func (r *postgresRepository) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
 
 }
--- golang/services/order/service.go
+++ golang/services/order/service.go
@@ -2,6 +2,11 @@ package order
 
 import "time"
 
+type Service interface {
+	PostOrder()
+	GetOrdersForAccount()
+}
+
 type Order struct {
 	ID         string
 	CreatedAt  time.Time
