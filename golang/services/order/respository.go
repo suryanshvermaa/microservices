c2aef6c76abaeb403e9cb8940a86099907325e7b
Suryansh Verma <154125921+suryanshvermaa@users.noreply.github.com>
2025-09-17 21:40:14 +0000

    feat: implement GetOrdersForAccount method with SQL query and order struct mapping

--- golang/services/order/respository.go
+++ golang/services/order/respository.go
@@ -69,5 +69,79 @@ func (r *postgresRepository) PutOrder(ctx context.Context, o Order) (err error) 
 }
 
 func (r *postgresRepository) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	rows, err := r.db.QueryContext(
+		ctx,
+		`SELECT
+      o.id,
+      o.created_at,
+      o.account_id,
+      o.total_price::money::numeric::float8,
+      op.product_id,
+      op.quantity
+    FROM orders o JOIN order_products op ON (o.id = op.order_id)
+    WHERE o.account_id = $1
+    ORDER BY o.id`,
+		accountID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
+	orders := []Order{}
+	order := &Order{}
+	lastOrder := &Order{}
+	orderedProduct := &OrderedProduct{}
+	products := []OrderedProduct{}
+
+	// Scan rows into Order structs
+	for rows.Next() {
+		if err = rows.Scan(
+			&order.ID,
+			&order.CreatedAt,
+			&order.AccountID,
+			&order.TotalPrice,
+			&orderedProduct.ID,
+			&orderedProduct.Quantity,
+		); err != nil {
+			return nil, err
+		}
+		// Scan order
+		if lastOrder.ID != "" && lastOrder.ID != order.ID {
+			newOrder := Order{
+				ID:         lastOrder.ID,
+				AccountID:  lastOrder.AccountID,
+				CreatedAt:  lastOrder.CreatedAt,
+				TotalPrice: lastOrder.TotalPrice,
+				Products:   products,
+			}
+			orders = append(orders, newOrder)
+			products = []OrderedProduct{}
+		}
+		// Scan products
+		products = append(products, OrderedProduct{
+			ID:       orderedProduct.ID,
+			Quantity: orderedProduct.Quantity,
+		})
+
+		*lastOrder = *order
+	}
+
+	// Add last order (or first :D)
+	if lastOrder != nil {
+		newOrder := Order{
+			ID:         lastOrder.ID,
+			AccountID:  lastOrder.AccountID,
+			CreatedAt:  lastOrder.CreatedAt,
+			TotalPrice: lastOrder.TotalPrice,
+			Products:   products,
+		}
+		orders = append(orders, newOrder)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return orders, nil
 }
--- golang/services/order/service.go
+++ golang/services/order/service.go
@@ -1,10 +1,13 @@
 package order
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Service interface {
-	PostOrder()
-	GetOrdersForAccount()
+	PostOrder(ctx context.Context, accountID string, products []OrderedProduct) error
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
 }
 
 type Order struct {
@@ -16,4 +19,25 @@ type Order struct {
 }
 
 type OrderedProduct struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	Quantity    uint32
+}
+
+type orderService struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &orderService{repository: r}
+}
+
+func (s *orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) error {
+
+}
+
+func (s *orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+
 }
