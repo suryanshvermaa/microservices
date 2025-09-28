f2299d07a01566de111da69f01d37163de4588c0
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-09-28 17:04:50 +0530

    Implement PostOrder method to create orders and retrieve order details

--- golang/services/order/client.go
+++ golang/services/order/client.go
@@ -1,6 +1,9 @@
 package order
 
 import (
+	"context"
+	"time"
+
 	"github.com/suryanshvermaa/microservices/golang/services/order/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -26,3 +29,35 @@ func NewClient(url string) (*Client, error) {
 func (c *Client) Close() {
 	c.conn.Close()
 }
+
+func (c *Client) PostOrder(ctx context.Context, accountId string, products []OrderedProduct) (*Order, error) {
+	protoProducts := []*pb.PostOrderRequest_OrderProduct{}
+	for _, p := range products {
+		protoProducts = append(protoProducts, &pb.PostOrderRequest_OrderProduct{
+			ProductId: p.ID,
+			Quantity:  p.Quantity,
+		})
+	}
+	r, err := c.service.PostOrder(
+		ctx,
+		&pb.PostOrderRequest{
+			AccountId: accountId,
+			Products:  protoProducts,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	newOrder := r.Order
+	newOrderCreatedAt := time.Time{}
+	newOrderCreatedAt.UnmarshalBinary(newOrder.CreatedAt)
+	return &Order{
+		ID:         newOrder.Id,
+		CreatedAt:  newOrderCreatedAt,
+		TotalPrice: newOrder.TotalPrice,
+		AccountID:  newOrder.AccountId,
+		Products:   products,
+	}, nil
+}
+
+func (c *Client) GetOrdersForAccount()
