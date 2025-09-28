b4a474589e381c18397bcfcfdaf0b238a167d66f
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-09-28 16:30:02 +0530

    Implement PostOrder method to handle product retrieval and order creation

--- golang/services/order/server.go
+++ golang/services/order/server.go
@@ -2,6 +2,7 @@ package order
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -45,11 +46,54 @@ func ListenGRPC(s Service, accountURL, catalogURL string, port int) error {
 	return serv.Serve(lis)
 }
 
-func (s *grpcServer) PostOrder(ctx context.Context, r *pb.PostOrderRequest) (*pb.PostOrderResponse, error)
-{
-	_,err:=s.accountClient.GetAccount(ctx,r.AccountId)
+func (s *grpcServer) PostOrder(ctx context.Context, r *pb.PostOrderRequest) (*pb.PostOrderResponse, error) {
+	_, err := s.accountClient.GetAccount(ctx, r.AccountId)
 	if err != nil {
-		log.Println("error in getting account:",err)
+		log.Println("error in getting account:", err)
 		return nil, err
 	}
+	productIds := []string{}
+	orderedProducts, err := s.catalogClient.GetProducts(ctx, 0, 0, productIds, "")
+	if err != nil {
+		log.Println("error in getting products:", err)
+		return nil, errors.New("product not found")
+	}
+	products := []OrderedProduct{}
+	for _, p := range orderedProducts {
+		product := OrderedProduct{
+			ID:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Quantity:    0,
+		}
+		for _, rp := range r.Products {
+			if rp.ProductId == p.ID {
+				products = append(products, product)
+			}
+		}
+	}
+	order, err := s.service.PostOrder(ctx, r.AccountId, products)
+	if err != nil {
+		log.Println("error posting order", err)
+		return nil, errors.New("could not post order")
+	}
+	orderProto := &pb.Order{
+		Id:         order.ID,
+		AccountId:  order.AccountID,
+		TotalPrice: order.TotalPrice,
+		Products:   []*pb.Order_OrderProduct{},
+	}
+	for _, p := range order.Products {
+		orderProto.Products = append(orderProto.Products, &pb.Order_OrderProduct{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Quantity:    p.Quantity,
+		})
+	}
+	return &pb.PostOrderResponse{
+		Order: orderProto,
+	}, nil
 }
