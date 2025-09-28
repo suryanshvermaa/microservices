8b4bcbfc8e99b18e3dc63344cab48315a25f712d
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-09-28 16:58:00 +0530

    Implement order client with gRPC connection and service initialization

--- golang/services/order/client.go
+++ golang/services/order/client.go
@@ -1,1 +1,28 @@
 package order
+
+import (
+	"github.com/suryanshvermaa/microservices/golang/services/order/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.OrderServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	c := pb.NewOrderServiceClient(conn)
+	return &Client{
+		conn:    conn,
+		service: c,
+	}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
